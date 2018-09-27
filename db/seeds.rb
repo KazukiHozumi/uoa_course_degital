e7182174469323d90a7ebb16544b1e50d7bb5c96
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-09-27 23:26:03 +0900

    クラスからデータベースを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,39 @@ Subcategory.create(subcategory_name: 's_name_2', category_id: 2)
 
 Course.create(course_code: 'code1', course_name: 'name1', subcategory_id: 1)
 Course.create(course_code: 'code2', course_name: 'name2', subcategory_id: 2)
+
+def createCategoryDB(category)
+  Category.create(category_name: category.category_name)
+  dummyCategory = Category.find(category_name: category.category_name)
+  category.subcategories.each { |subcategory|
+    CreateSubcategoryDB(dummyCategory.id, subcategory)
+  }
+end
+
+def createSubcategoryDB(category_id, subcategory)
+  Subcategory.create(subcategory.subcategory_name, category_id)
+  dummySubcategory = Subcategory.find(subcategory_name: subcategory.subcategory_name)
+  subcategory.course_list.each { |course|
+    CreateCourseDB(dummySubcategory.id, course)
+  }
+end
+
+def createCourseDB(subcategory_id, course)
+  Course.create(course_code: course.course_code, course_name: course.course_name, subcategory_id: subcategory_id)
+  dummyCourse = Course.find(course_name: course.course_name)
+  course.teachers.each { |teacher|
+    if Teather.find(teacher_name: teacher) == nil then
+      createTeacherDB(teacher)
+    end
+    dummyTeacher = Teacher.find(teacher_name: teacher.teacher_name)
+    createCourse_teacherDB(course_id: dummyCourse.id, teacher_id: dummyTeacher.id)
+  }
+end
+
+def createTeacherDB(teacher)
+  Teacher.create(teacher_name: teacher)
+end
+
+def createCourse_teacherDB(course_id, teacher_id)
+  CourseTeacher.create(course_id: course_id, teacher_id: teacher_id)
+end
