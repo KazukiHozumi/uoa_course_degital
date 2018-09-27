dbc07d2156371bd20be60083a3730bcda3993f7f
ryoya_ito <horusu.dqx@gmail.com>
2018-09-27 20:14:05 +0900

    コンフリクト解消，ファイル構造変更

--- app/service/data_collect_service.rb
+++ app/service/data_collect_service.rb
@@ -1,9 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-require './category'
+require '../dto/category'
+require '../dto/subcategory'
 
-class DataCollector
+class DataCollectService
   def get_courses
 
     syllabus_url = 'http://web-ext.u-aizu.ac.jp/official/curriculum/syllabus/1_J_000.html'
@@ -15,17 +16,21 @@ class DataCollector
     doc = Nokogiri::HTML.parse(html, nil, 'UTF-8')
     doc.xpath('//div[@class="box"]').each do |box|
       category = Category.new(box.css('td.daikbn').inner_text.strip)
-      box.xpath('//td[@class="chukbn"]').each do |subcategory|
-        p subcategory.inner_text.strip
+      count = 0
+      box.xpath('./div//tr').each do |tr|
+        if tr.xpath('.//table[@class="chu-inner"]')
+
+        end
       end
       categorys << category
     end
 
     categorys.each do |category|
       p category.category_name
+      p category.subcategories
     end
 
   end
 end
 
-DataCollector.new.get_courses
+DataCollectService.new.get_courses
