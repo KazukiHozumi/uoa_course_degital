2f03c4825f350b9ec9a229cf368d92bac94a6bde
ryoya_ito <horusu.dqx@gmail.com>
2018-09-28 15:30:29 +0900

    確認用情報を動的に追加する処理実装途中

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -10,17 +10,7 @@ $(function() {
     $('#confirm').on('click', function(){
         const query = document.location.search.substring(1);
 
-        let parameters = query.split('&');
-        let result = [];
-        for (let i = 0; i < parameters.length; i++) {
-
-            // パラメータ名とパラメータ値に分割する
-            let element = parameters[i].split('=');
-
-            let paramValue = decodeURIComponent(element[1]);
-            result[i] = paramValue
-
-        }
+        let result = getParameterValues(query);
         window.location.href=`detail?studentId=${result[0]}&name=${result[1]}`+
             `&selectedCourses=${selectedCoueses.join(',')}`
     });
@@ -30,4 +20,18 @@ $(function() {
             selectedCoueses.push(this.nextElementSibling.value.replace('>', ''))
         }
     });
+
+    function getParameterValues(query){
+        let parameters = query.split('&');
+        let result = [];
+        for (let i = 0; i < parameters.length; i++) {
+
+            // パラメータ名とパラメータ値に分割する
+            let element = parameters[i].split('=');
+
+            let paramValue = decodeURIComponent(element[1]);
+            result[i] = paramValue
+        }
+        return result;
+    }
 });
